2a77945d511cbd615c807c4004854afd21d6b368
manojmanu25 <manoj.maduri1@gmail.com>
2017-03-30 23:36:37 -0400

    included semantic and angular2-moment
    i did include the semantic and succeded in importing it to the project but angular2-moment implementing made me sick :(

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import { AppComponent1 } from './app.component';
 
-describe('AppComponent','AppComponent1', () => {
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,AppComponent1
+        AppComponent
       ],
     }).compileComponents();
   }));
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { MomentModule } from 'angular2-moment';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+//import * as moment from '../node_modules/angular2-moment/';
+//import '../node_modules/angular2-moment/add.pipe.d.ts';
+//const moment: moment.MomentStatic = (<any>moment_)['default'] || moment_;
 
 platformBrowserDynamic().bootstrapModule(AppModule);
